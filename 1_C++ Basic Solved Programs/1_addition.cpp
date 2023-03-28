e935c1c705478d432f01c5fda9e04956fd8ee8ff
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-29 00:52:46 +0600

    28-03-2023

---
+++ 1_C++ Basic Solved Programs/1_addition.cpp
@@ -0,0 +1,20 @@
+/*  C++ Program to Addition of Two Numbers  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first, second, sum;
+
+    cout << "Enter 1st integer :: ";
+    cin >> first;
+    cout << "\nEnter 2nd integer :: ";
+    cin >> second;
+
+    sum = first + second;
+
+    cout<<"\nSum of Two Numbers [ "<<first<<" + "<<second<<" ] = "<<sum<<"\n";
+
+    return 0;
+}
