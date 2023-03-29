9d5378c53aa951ce202c5d08328dd5a564ddd290
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-29 12:15:44 +0600

    29-03-2023

---
+++ 1_C++ Basic Solved Programs/3_sizeof.cpp
@@ -0,0 +1,17 @@
+/*  C++ Program to Find Size of Int Float Double and Char data types  */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+
+    cout << "size if int ::"<<sizeof(int) <<" bytes "<<endl;
+     cout << "size if char ::"<<sizeof(char) <<" bytes "<<endl;
+      cout << "size if float ::"<<sizeof(float) <<" bytes "<<endl;
+       cout << "size if double ::"<<sizeof(double) <<" bytes "<<endl;
+   
+
+  
+    return 0;
+}
